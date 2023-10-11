93f2de26de06a7825b209995e30f40fc5ca1b3ad
giuliafallati <giulia.fallati@gmx.ch>
2023-10-11 08:52:12 +0200

    vor überarbeiten

--- js/index.js
+++ js/index.js
@@ -10,13 +10,14 @@ goButton.onclick = function () {
     const tableElement = document.createElement('table');
     body.appendChild(tableElement);
 
-    var tableRowElement = document.createElement('tr');
+    var tableRowElement = document.createElement('tr'); 
     var myRow = new Number(rowInput.value);
     var myColumn = new Number(columnInput.value);
     var numberOfColumns = 0;
     var numberOfCells = 1;
     var calculatedNumberOfCells = myRow * myColumn;
     var columnCounter = 1;
+    var idOfCell = 1;
 
     //Columns einfuegen
     do {
@@ -52,26 +53,19 @@ goButton.onclick = function () {
         }
     }
 
-    var idOfCell = 1;
-    var isPrimeNumber = 2;
-
-
     //Button für Feld markieren
     nextStepButton.onclick = function () {
 
-        if (isPrimeNumber <= calculatedNumberOfCells) {
+        if (idOfCell <= calculatedNumberOfCells) {
 
             //Feld mit Nr. 1 verstecken
             if (idOfCell == 1) {
                 var hideNrOne = document.getElementById('id1');
                 hideNrOne.style.visibility = "hidden";
-                idOfLastCell = calculatedNumberOfCells;
-                var notRedCell = document.getElementById('id' + idOfLastCell);
-                notRedCell.style.backgroundColor = "rgb(255, 239, 204)";
             }
 
             //Felder anders einfaerben
-            else if (idOfCell != 1) {
+            else {
 
                 var idOfLastCell = idOfCell - 1;
 
@@ -79,46 +73,16 @@ goButton.onclick = function () {
                 var redCell = document.getElementById('id' + idOfCell);
                 redCell.style.backgroundColor = "rgb(255, 220, 144)";
 
-                if (idOfCell % isPrimeNumber == 0 && idOfCell != isPrimeNumber) {
-                    var hidePrimeNumber = document.getElementById('id' + idOfCell);
-                    hidePrimeNumber.style.visibility = "hidden";
-                }  
-            }
-
-            if(document.getElementById('id' + isPrimeNumber + 1) === 'hidden') {
-                do {
-                    isPrimeNumber += 1;
+                //Das vorherige Feld wieder normal einfaerben
+                if ((idOfCell != 1 && idOfCell != 2) || idOfCell == calculatedNumberOfCells) {
+                    var notRedCell = document.getElementById('id' + idOfLastCell);
+                    notRedCell.style.backgroundColor = "rgb(255, 239, 204)";
                 }
-                while (document.getElementById('id' + isPrimeNumber + 1) === 'hidden')
-            }
-            else if (idOfCell == calculatedNumberOfCells) {
-                
-             
-                isPrimeNumber += 1;
+
+                idOfLastCell += 1;
             }
 
-            //Das vorherige Feld wieder normal einfaerben
-            if (idOfCell != 2) {
-                var notRedCell = document.getElementById('id' + idOfLastCell);
-                notRedCell.style.backgroundColor = "rgb(255, 239, 204)";
-            }
-
-            idOfLastCell += 1;
-        }
-
-        if (idOfCell == calculatedNumberOfCells) {
-            
-
-            idOfCell = 1;
-        }
-        else {
             idOfCell += 1;
         }
     }
 }
-
-// isPrimeNumber += 1;
-// while (hidePrimeNumber.style.visibility === "hidden") {
-//     isPrimeNumber += 1
-//     idOfCell += 1;
-// }
